002543405ddb40d721513561a675d0bf489c0898
azaremde <40906347+azaremde@users.noreply.github.com>
2019-06-04 21:42:44 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -41,22 +41,71 @@ TooltipHandler = () => {
 }
 
 PlusHandler = () => {
-    let bloggers = $('.table-blogger');
+    let bloggers = $('.blogger');
     let tableOpen = new Array(bloggers.length).fill(false);
 
     $.map(bloggers, (element, i) => {
         $(element).find('.table-blogger__open-copies').on('click', () => {
             if(!tableOpen[i]) {
-                $(element).addClass('table-blogger--loading');
+                $(element).addClass('blogger--loading');
                 setTimeout(() => {
-                    $(element).removeClass('table-blogger--loading');
-                    $(element).addClass('table-blogger--open');
+                    $(element).removeClass('blogger--loading');
+                    $(element).addClass('blogger--open');
                 }, 1000);
                 tableOpen[i] = true;
             } else {
-                $(element).removeClass('table-blogger--open');
+                $(element).removeClass('blogger--open');
                 tableOpen[i] = false;
             }
+        });
+    });
+}
+
+SetTableWidth = () => {
+    let cols = [
+        $('.table-col--1'),
+        $('.table-col--2'),
+        $('.table-col--3'),
+        $('.table-col--4'),
+        $('.table-col--5'),
+        $('.table-col--6'),
+        $('.table-col--7'),
+        $('.table-col--8'),
+        $('.table-col--9'),
+        $('.table-col--10'),
+        $('.table-col--11'),
+        $('.table-col--12'),
+        $('.table-col--13'),
+    ];
+
+    let biggestWidth = [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    ];
+    
+    $.map(cols, (element, i) => {
+        $.map($(element), (_element) => {
+            if(parseInt($(_element).css('width')) > biggestWidth[i] ) {
+                biggestWidth[i] = parseInt($(_element).width());
+            }
+        });
+    });
+
+    $.map(cols, (element, i) => {
+        $(element).css({
+            'max-width' : biggestWidth[i] + 'px',
+            'min-width' : biggestWidth[i] + 'px'
         });
     });
 }
@@ -65,4 +114,5 @@ $(document).ready(() => {
     HeaderLoad();
     TooltipHandler();
     PlusHandler();
+    SetTableWidth();
 });
